c045ff40a5f61c76007115434467251b02f2808c
HoAnhTu245 <hoanhtu30112005@gmail.com>
2024-03-13 14:56:37 +0700

    ..

---
+++ Week4/PartA/Bai5/Bai5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int N = 100; // nếu thêm const thì khi chuỗi có độ dài > N sẽ lỗi
+int main()
+{
+    char a[5] = "abcd";
+    cout << "_" << a << "_____";
+
+    return 0;
+}
